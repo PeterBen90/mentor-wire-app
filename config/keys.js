98ea752737f804ac759d29b9f1186a33de602a8e
Peter Benvenuto <peterbenvenuto90@yahoo.ca>
2018-06-16 12:57:33 -0400

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://Peter:Password1@ds263710.mlab.com:63710/mentor-wire-db"
+};
---
+++ routes/api/posts.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const router = express.Router();
+
+//@route   GET request to api/posts/test
+//@desc    Tests posts route
+//@access  Public route
+router.get('/test', (req, res) => res.json({msg: "Posts Works"}));
+
+module.exports = router;
