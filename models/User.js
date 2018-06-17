30c2a3cdb300aa9756b2ae7fa75403418cbb6aa1
Peter Benvenuto <peterbenvenuto90@yahoo.ca>
2018-06-17 18:11:28 -0400

    user added to database with hashed password

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 //Create Schema
@@ -16,8 +16,7 @@ const UserSchema = new Schema({
     required: true
   },
   avatar: {
-    type: String,
-    required: true
+    type: String
   },
   date: {
     type: Date,
@@ -25,6 +24,6 @@ const UserSchema = new Schema({
   }
 });
 
-const User = mongoose.model("users", UserSchema);
+const User = mongoose.model('users', UserSchema);
 
 module.exports = { User };
